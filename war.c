3d85c6282d8970b9895c965d7061bb350ac81b50
hyagosrs <hyagosilvasrs@gmail.com>
2026-01-21 18:20:33 -0300

    Struct inicial

--- war.c
+++ war.c
@@ -26,7 +26,11 @@
 
 // --- Estrutura de Dados ---
 // Define a estrutura para um território, contendo seu nome, a cor do exército que o domina e o número de tropas.
-
+struct Territorio {
+    char Nome [30];
+    char Cor[10];
+    int Tropas;
+}
 // --- Protótipos das Funções ---
 // Declarações antecipadas de todas as funções que serão usadas no programa, organizadas por categoria.
 // Funções de setup e gerenciamento de memória:
